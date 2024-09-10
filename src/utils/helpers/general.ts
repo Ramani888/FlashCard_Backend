629123879af9f9dae1ba83d77499f76659395d48
Divyeshrzeusint <divyeshr@zeusint.com>
2024-09-10 21:02:02 +0530

    update otp digit

--- src/utils/helpers/general.ts
+++ src/utils/helpers/general.ts
@@ -1,3 +1,7 @@
+// export const generateOTP = () => {
+//     return Math.floor(100000 + Math.random() * 900000).toString();
+// }
+
 export const generateOTP = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
+    return Math.floor(1000 + Math.random() * 9000).toString();
 }
