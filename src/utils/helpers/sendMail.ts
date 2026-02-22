711e995c383e1449c548acdc9471bc9f3e5e4f8c
Ramani Divyesh <109016287+Ramani888@users.noreply.github.com>
2026-02-22 14:14:08 +0530

    send mail is update

--- src/utils/helpers/sendMail.ts
+++ src/utils/helpers/sendMail.ts
@@ -1,34 +1,38 @@
 import nodemailer from 'nodemailer';
 
-const sendMail = async (to: string, subject: string, htmlTemplate: any, imageUrl?: string) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            // user: 'divyeshr@zeusint.com',
-            // pass: 'xpqn mysr vrkg aifj'
-
-            user: 'Biblestudykitapp@gmail.com',
-            pass: 'jtlo jveb dilx fhlx'
-        }
-    });
-
-    const mailOptions: { [key: string]: any } = {
-        from: 'Biblestudykitapp@gmail.com',
-        to,
-        subject,
-        html: htmlTemplate
-    };
-
-    // Add attachment only if imagePath is provided
-    if (imageUrl) {
-        mailOptions.attachments = [
-            {
-                path: imageUrl
-            }
-        ];
+const sendMail = async (
+  to: string,
+  subject: string,
+  htmlTemplate: any,
+  imageUrl?: string
+) => {
+  const transporter = nodemailer.createTransport({
+    host: 'mail.privateemail.com',
+    port: 587, // TLS
+    secure: false, // false for 587, true for 465
+    auth: {
+      user: 'support@biblestudycards.app',
+      pass: 'BibleApp1234$' // mailbox or app password
     }
+  });
 
-    await transporter.sendMail(mailOptions);
+  const mailOptions: any = {
+    from: '"Bible Study Cards" <support@biblestudycards.app>',
+    to,
+    subject,
+    html: htmlTemplate
+  };
+
+  if (imageUrl) {
+    mailOptions.attachments = [
+      {
+        filename: 'image.jpg',
+        path: imageUrl
+      }
+    ];
+  }
+
+  await transporter.sendMail(mailOptions);
 };
 
 export default sendMail;
